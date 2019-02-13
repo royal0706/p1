aa3d1e76b1da20415ce34c145dc3e23a2c4ee52c
royal0706 <victoria_royal@harvard.edu>
2019-02-12 22:14:11 -0500

    Added index file with an image.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Jane Austen</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<div class="container">
+
+		<h1>Jane Austen</h1>
+
+		<img src='images/austen.jpg' alt='Jane Austen'>
+
+		<h2>About Jane</h2>
+		<p>Jane Austen was an English novelist known primarily for her six major novels, which interpret, critique and comment upon the British landed gentry at the end of the 18th century. Austen's plots often explore the dependence of women on marriage in the pursuit of favourable social standing and economic security. Her works critique the novels of sensibility of the second half of the 18th century and are part of the transition to 19th-century literary realism. Her use of biting irony, along with her realism, humour, and social commentary, have long earned her acclaim among critics, scholars, and popular audiences alike.</p>
+
+		<h2>Random Quote</h2>
+		<blockquote>
+		 It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.
+		</blockquote>
+
+	</div>
+
+</body>
+</html>
